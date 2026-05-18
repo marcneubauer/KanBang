7c5f7ee30ddae9e3836219ce209df3c8f9551df3
marc.neubauer <marc.neubauer@gmail.com>
2026-05-18 11:11:00 -0600

    fix error lint

--- eslint.config.js
+++ eslint.config.js
@@ -38,7 +38,7 @@ export default tseslint.config(
     },
   },
   {
-    files: ['**/*.svelte'],
+    files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
     languageOptions: {
       parser: svelteParser,
       parserOptions: {
